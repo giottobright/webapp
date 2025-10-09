970699d6708d9e35abe222d398d6aa304885c5a5
dmitrii <soloaskme@gmail.com>
2025-10-09 21:15:34 +0300

    123

--- src/personas.js
+++ src/personas.js
@@ -4,7 +4,7 @@ export const PERSONAS = [
   { code: 'melis', name_tr: 'Melis', name_ru: 'Мелис', tagline_tr: 'Zeki sohbetçi', tagline_ru: 'Умная собеседница' },
   { code: 'derya', name_tr: 'Derya', name_ru: 'Дерья', tagline_tr: 'Tutkulu ve kendinden emin', tagline_ru: 'Страстная и уверенная' },
   { code: 'irem', name_tr: 'İrem', name_ru: 'Ирем', tagline_tr: 'Yaratıcı hayalperest', tagline_ru: 'Творческая мечтательница' },
-  { code: 'ayse', name_tr: 'Ayşe', name_ru: 'Айше', tagline_tr: 'Anadolu sıcaklığı, ev gibi', tagline_ru: 'Своя в доску (Anadolu sıcaklığı)' },
+  { code: 'ayse', name_tr: 'Ayşe', name_ru: 'Айше', tagline_tr: 'Sporcu ve motivasyonel', tagline_ru: 'Спортивная и мотивирующая' },
   { code: 'eylul', name_tr: 'Eylül', name_ru: 'Эйлюль', tagline_tr: "İstanbul influencer'ı", tagline_ru: 'Инфлюенсер из Стамбула' },
   { code: 'leyla', name_tr: 'Leyla', name_ru: 'Лейла', tagline_tr: 'Gamer/teknoloji meraklısı', tagline_ru: 'Геймерша/техно-гикуша' },
 ]
